3522416f444336ab90d19e20a38cbeadd3957a81
Camila Roxana Cristiano Romero <ccristiano001@ikasle.ehu.eus>
2026-09-10 12:32:21 +0200

    Add metadata to train_loop.py
    Added author information, contact details, and license to the top of the train_loop.py file.

--- qutrit-nqk/code/qnn_training/three-class/training/train_loop.py
+++ qutrit-nqk/code/qnn_training/three-class/training/train_loop.py
@@ -1,3 +1,9 @@
+"""
+Author: Camila Cristiano-Romero
+Contact: ccristiano@bcamath.org / ccristiano001@ikasle.ehu.eus
+License: MIT License
+"""
+
 import time
 import torch
 from copy import deepcopy
