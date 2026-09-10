59dbde157be403bb4785530f2c283804ea624024
Camila Roxana Cristiano Romero <ccristiano001@ikasle.ehu.eus>
2026-09-10 12:30:28 +0200

    Add metadata to predict.py
    Added author information, contact details, and license to the predict.py file.

--- qutrit-nqk/code/qnn_training/binary/utils/predict.py
+++ qutrit-nqk/code/qnn_training/binary/utils/predict.py
@@ -1,3 +1,9 @@
+"""
+Author: Camila Cristiano-Romero
+Contact: ccristiano@bcamath.org / ccristiano001@ikasle.ehu.eus
+License: MIT License
+"""
+
 def predict_Lz_Binary(output):
     return (output > 0).long()
 
